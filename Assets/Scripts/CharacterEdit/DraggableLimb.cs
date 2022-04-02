54d6c372373fe424ab26fe776011eac6fa77aadc
Michael Freaney <michael.freaney324@gmail.com>
2022-04-02 10:22:28 -0500

    uhhhh

--- Assets/Scripts/CharacterEdit/DraggableLimb.cs
+++ Assets/Scripts/CharacterEdit/DraggableLimb.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class DraggableLimb : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    bool beingHeld = false;
+    Vector3 mousePos = Vector3.zero;
+
+    private void OnDrawGizmos()
     {
+        Gizmos.color = Color.cyan;
+
+        Gizmos.DrawSphere(mousePos, 0.5f);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+         mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Debug.Log("Distance from limb to mouse: " + Vector3.Distance(mousePos, transform.position));
+
+        //if mouse 0 and in vicinity of the limb
+        if (Input.GetMouseButtonDown(0) && Vector3.Distance(mousePos, transform.position) < 1.0f)
+        {
+            beingHeld = true;
+
+            Cursor.visible = false;
+        }
+
+
+        if (Input.GetMouseButtonUp(0) && beingHeld)
+        {
+            beingHeld = false;
+
+            Cursor.visible = true;
+        }
+
+
+        if (beingHeld)
+        {
+            //snap to mouse position
+        }
     }
 }
